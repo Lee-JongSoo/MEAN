b38a960a89bf15114c271e0f79ecc7313f23d38d
Lee Jong Soo <jongsu2645@gmail.com>
2021-09-22 17:10:20 +0900

    Update Main page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* Get homepage */
 const index = (req, res) => {
-    res.render('index', {title: 'Express by 2017250035 Lee-JOngSu'})
+    res.render('index', {title: 'Express '})
 };
 
 module.exports = {
