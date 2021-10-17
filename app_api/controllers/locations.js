4b2c472beb9f1137e5f0cd04537e85bc4c333496
Lee Jong Soo <jongsu2645@gmail.com>
2021-10-18 01:03:11 +0900

    Writing a REST API-3

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -68,8 +68,82 @@ const locationListByDistance = async (req, res) => {
     }
 };
 
-const locationsCreate = (req, res) => {};
-const locationsUpdateOne = (req, res) => {};
+const locationsCreate = (req, res) => {
+    Loc.create(
+      {
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [
+          {
+            days: req.body.days1,
+            oepning: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+          },
+          {
+            days: req.body.days2,
+            oepning: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+          },
+        ],
+      },
+      (err, location) => {
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(201).json(location);
+        }
+      }
+    );
+  };
+
+  const locationsUpdateOne = (req, res) => {
+    if (!req.params.locationid) {
+      return res
+        .status(404)
+        .json({ message: "Not found, locationid is required" });
+    }
+  
+    Loc.findById(req.params.locationid)
+      .select("-reviews -rating")
+      .exec((err, location) => {
+        if (!location) {
+          return res.status(404).json({ message: "locationid not found" });
+        } else if (err) {
+          return res.status(400).json(err);
+        }
+  
+        location.name = req.body.name;
+        location.address = req.body.address;
+        location.facilities = req.body.facilities.split(",");
+        location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+        (location.openingTimes = [
+          {
+            days: req.body.days1,
+            oepning: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+          },
+          {
+            days: req.body.days2,
+            oepning: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+          },
+        ]),
+          location.save((err, loc) => {
+            if (err) {
+              res.status(404).json(err);
+            } else {
+              res.status(200).json(loc);
+            }
+          });
+      });
+  };
+  
 const locationsDeleteOne = (req, res) => {};
 
 module.exports = {
--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -1,54 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema( {
-  author: String,
+const reviewSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
   rating: {
     type: Number,
     required: true,
     min: 0,
-    max: 5
+    max: 5,
   },
-  reviewText: String,
+  reviewText: {
+    type: String,
+    required: true,
+  },
   createdOn: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-const openingTimeSchema = new mongoose.Schema( {
+const openingTimeSchema = new mongoose.Schema({
   days: {
     type: String,
-    required: true
+    required: true,
   },
   opening: String,
   closing: String,
   closed: {
     type: Boolean,
-    required: true
-  }
+    required: true,
+  },
 });
 
-const locationSchema = new mongoose.Schema ( {
+const locationSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   address: String,
   rating: {
     type: Number,
-    'default': 0,
+    default: 0,
     min: 0,
-    max: 5
+    max: 5,
   },
   facilities: [String],
-  coords: {
-    type: {type: String},
-    coordinates: [Number]
-  },
+  coords: { type: [Number], index: "2dsphere" },
   openingTimes: [openingTimeSchema],
-  reviews: [reviewSchema]
+  reviews: [reviewSchema],
 });
 
-locationSchema.index({coords: '2dsphere'});
+locationSchema.index({ coords: "2dsphere" });
 
-mongoose.model('Location', locationSchema);
+module.exports = mongoose.model("Location", locationSchema);
