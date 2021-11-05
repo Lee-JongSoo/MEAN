36440a064a02e7274fb563afaddcc45046eda419
Lee Jong Soo <jongsu2645@gmail.com>
2021-11-05 14:15:11 +0900

    make it better

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = `mongodb+srv://my_atlas_user:ltyxodus7!@cluster0.0g2uj.mongodb.net`;
+const dbURI = `mongodb+srv://my_atlas_user:ltyxodus7@cluster0.0g2uj.mongodb.net`;
 mongoose.connect(dbURI, {useNewUrlParser: true}); 
 
 mongoose.connection.on('connected', () => {
