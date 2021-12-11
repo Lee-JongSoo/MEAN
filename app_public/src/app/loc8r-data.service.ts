13395b5ab7211e65467cfda8bdd6d8a138b57a0b
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2021-12-12 05:24:06 +0900

    make it better

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -15,8 +15,8 @@ export class Loc8rDataService {
     @Inject(BROWSER_STORAGE) private storage: Storage
   ) {}
 
-  private apiBaseUrl = 'http://localhost:3000/api';
-  //private apiBaseUrl = 'https://findlocation97.herokuapp.com/api';
+  //private apiBaseUrl = 'http://localhost:3000/api';
+  private apiBaseUrl = 'https://findlocation97.herokuapp.com/api';
 
   public getLocations(lat: number, lng: number): Promise<Location[]> {
   //const lng: number = 127.26939808664095;
